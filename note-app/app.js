978d4e6f3b02d233fec7030938996452717edcc2
Seungho Choi <ksngho7@gmail.com>
2019-10-17 19:32:40 -0400

    note-app

---
+++ note-app/app.js
@@ -0,0 +1,13 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
+const getNotes = require('./notes.js');
+
+const command = process.argv[2];
+
+
+if(command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove'){
+  console.log('Removing note!');
+}
